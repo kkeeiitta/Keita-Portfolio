c991e8b3d6d34a420d90925c2ff6d57964f6fc61
dauch <keita.uchida.0405@gmail.com>
2026-06-02 18:22:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+ScrollReveal({
+  reset: true
+});
+
+ScrollReveal().reveal('.logo', {
+  delay: 200,
+  origin: 'top',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.portfolioInfo', {
+  delay: 500,
+  origin: 'left',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.body img', {
+  delay: 600,
+  origin: 'right',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.about img', {
+  delay: 300,
+  origin: 'left',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.about-text', {
+  delay: 500,
+  origin: 'right',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.title', {
+  delay: 500,
+  origin: 'top',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.card', {
+  delay: 400,
+  interval: 300
+});
+
+ScrollReveal().reveal('.contact p', {
+  delay: 300,
+  origin: 'top',
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.contact-button', {
+  delay: 500,
+  origin: 'bottom',
+  distance: '60px'
+});
